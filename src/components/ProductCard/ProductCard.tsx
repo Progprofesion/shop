82ab3caa70d44c651034bf8dc14afa1fa4363ded
Dmitriy <tiemposilencioo@gmail.com>
2025-03-25 06:33:12 +0300

    fixNameClass

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -17,20 +17,10 @@ const ProductCard = ({ product }: Iitem) => {
   const items = useCartItems();
   console.log("store111", items);
   return (
-    <article className="product" itemScope itemType="http://schema.org/Product">
-      <button onClick={() => useCartStore.getState().addItem(product)}>
-        Добавить в корзину
-      </button>
-      <header className="product-header">
-        <h2 className="product-title" itemProp="name">
-          {product.title} Заголовок
-        </h2>
-        <div
-          className="product-price"
-          itemProp="offers"
-          itemScope
-          itemType="http://schema.org/Offer"
-        >
+    <article className="product">
+      <header className="product__header">
+        <h2 className="product__title">{product.title} Заголовок</h2>
+        <div className="product__price">
           <meta itemProp="priceCurrency" content="RUB" />
           <span itemProp="price" content={product.price.toString()}>
             {product.price}
@@ -38,14 +28,15 @@ const ProductCard = ({ product }: Iitem) => {
         </div>
       </header>
 
-      <figure className="product-image">
+      <figure className="product__image">
         картинка
         <img
+          className=""
           src={product.image}
           alt="Красная футболка с хлопковым материалом"
           itemProp="image"
-          width="600"
-          height="400"
+          width="400"
+          height="250"
           loading="lazy"
         />
         <figcaption className="visually-hidden">
@@ -53,47 +44,11 @@ const ProductCard = ({ product }: Iitem) => {
         </figcaption>
       </figure>
 
-      <section className="product-description" aria-labelledby="desc-heading">
-        <h3 id="desc-heading" className="visually-hidden">
-          Описание товара
-        </h3>
-        <p itemProp="description">{product.description}</p>
+      <section className="product__description">
+        <h3 className="visually-hidden">Описание товара</h3>
+        <p>{product.description}</p>
       </section>
-
-      <section className="product-variants" aria-labelledby="variants-heading">
-        <h3 id="variants-heading" className="visually-hidden">
-          Доступные варианты
-        </h3>
-
-        <div className="variant-selector">
-          <label htmlFor="size-select">Размер</label>
-          {/* <select
-            id="size-select"
-            name="size"
-            className="variant-select"
-            aria-describedby="size-help"
-          >
-            <option value="">Выберите размер</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-          </select> */}
-          <span id="size-help" className="help-text">
-            Таблица размеров
-          </span>
-        </div>
-
-        <div className="variant-selector">
-          <label htmlFor="color-select">Цвет</label>
-          {/* <select id="color-select" name="color" className="variant-select">
-            <option value="">Выберите цвет</option>
-            <option value="red" data-color-hex="#ff0000">
-              Красный
-            </option>
-          </select> */}
-        </div>
-      </section>
-
-      <footer className="product-footer">
+      <footer className="product__footer">
         <button
           type="button"
           className="buy-button"
