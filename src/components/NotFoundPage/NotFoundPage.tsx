48021ce1f84ecb9d36bd778854682778d6bb1549
Dmitriy <tiemposilencioo@gmail.com>
2025-03-26 18:55:13 +0300

    fix 404 page for vercel

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,15 @@
 const NotFoundPage = () => {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
+    <div
+      style={{
+        textAlign: "center",
+        padding: "50px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+      }}
+    >
       <h1>404</h1>
       <p>Страница не найдена</p>
     </div>
